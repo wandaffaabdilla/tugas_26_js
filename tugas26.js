5c5b0f67d12a2bad7066d672e20050555f1fff18
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-03-23 17:38:26 +0800

    Tugas 26 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas26.js
@@ -0,0 +1,13 @@
+function tugas26() {
+  var data = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+  var data2 = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+
+  if ((data = false));
+  var data3 = " NOT Infinity.";
+
+  var data4 = data2.forEach(function (item) {
+    console.log("Angka " + item + data3);
+  });
+  return data4;
+}
+tugas26();
